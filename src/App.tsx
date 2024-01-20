381fe7ec64f35300059f0c01b6c6c0df0bf877b5
Tom Van Dort <tom@vandort.dev>
2024-01-19 20:19:06 -0500

    Add tailwind.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,118 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+import React from "react";
+
+interface CardProps {
+  title?: React.ReactNode;
+  children?: React.ReactNode;
+}
+
+function Card({ title, children }: CardProps) {
+  return (
+    <div className="rounded border border-gray-300 flex flex-col mb-2 px-4 py-4 max-w-4xl">
+      <div className=" text-sm font-medium">{title}</div>
+      <p className="text-gray-700 text-base flex justify-center">{children}</p>
+    </div>
+  );
+}
+
+interface PlayerInput {
+  name: string;
+  currentHp: number;
+  maxHp: number;
+}
+
+function PlayersInput() {
+  const [players, setPlayers] = useState<PlayerInput[]>([]);
+
+  const handleAddPlayerClick = () => {
+    setPlayers((previous) => [
+      ...previous,
+      { name: "", currentHp: 0, maxHp: 0 },
+    ]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <Card
+      title={
+        <div className="flex justify-between items-center">
+          <span className="flex ">Players</span>
+          <button
+            className="bg-green-600 py-1 px-3 text-white border-solid border-1 border-black rounded-lg"
+            onClick={handleAddPlayerClick}
+          >
+            Add
+          </button>
+        </div>
+      }
+    >
+      {players.length > 0 ? (
+        <hr className="my-4 border-gray-300" />
+      ) : (
+        <button className="hover:underline" onClick={handleAddPlayerClick}>
+          Add a player
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      )}
+
+      {players.length > 0 ? (
+        <div className="flex flex-wrap gap-y-8">
+          {players.map((player, playerIndex) => (
+            <PlayerInput
+              key={playerIndex}
+              playerIndex={playerIndex}
+              player={player}
+              placeholder={`Player ${playerIndex + 1}`}
+            />
+          ))}
+        </div>
+      ) : null}
+    </Card>
+  );
+}
+
+function PlayerInput({
+  player,
+  placeholder,
+  playerIndex,
+}: {
+  player: PlayerInput;
+  placeholder: string;
+  playerIndex: number;
+}) {
+  const [playerName, setPlayerName] = useState(player.name);
+  const id = (tag: string) => `player-input-${playerIndex}-${tag}`;
+  const nameId = id("name");
+  const currentHpId = id("currentHp");
+  const maxHpId = id("maxHp");
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex flex-col">
+        <label htmlFor={nameId}>Player name</label>
+        <input
+          type="text"
+          id={nameId}
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+          placeholder={placeholder}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="flex flex-wrap">
+        <input type="number" id={currentHpId} placeholder="Current HP" />
+        /
+        <input type="number" id={maxHpId} placeholder="Max HP" />
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="p-8 flex flex-col gap-y-4">
+      <h1 className="text-3xl font-bold">Goodberries!</h1>
+      <PlayersInput />
+    </div>
   );
 }
 
